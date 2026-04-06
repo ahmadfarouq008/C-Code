1e48521fa5f1ee47a7a1fb38b3b495577d4a8619
ahmadfarouq008 <ahmadfarouq008@gmail.com>
2026-04-06 17:16:41 +0500

    find leap year

--- 4.cpp
+++ 4.cpp
@@ -3,6 +3,19 @@ using namespace std;
 
 int main() {
 
+    int year = 0;
+
+    cout << "Enter a year: ";
+    cin >> year;
+
+    if (year % 4 == 0) {
+
+        cout << year << " is a leap year." << endl;
+    }
+    else {
+        cout << year << " is not a leap year." << endl;
+    }
+
     return 0;
 
 }
