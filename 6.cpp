adf8e6c9882b4b5244da95c32d1c54b0504e628c
ahmadfarouq008 <ahmadfarouq008@gmail.com>
2026-04-07 01:27:11 +0500

    type of triangle

--- 6.cpp
+++ 6.cpp
@@ -3,26 +3,22 @@ using namespace std;
 
 int main() {
 
-    int side1 = 0;
-    int side2 = 0;
-    int side3 = 0;
+    int side1=0;
+    int side2=0;
+    int side3=0;
 
     cout << "Enter three side lengths: ";
-    cin >> side1 >> side2 >> side3 ;
+    cin >> side1 >> side2 >> side3;
 
-    if(side1 == side2 && side2 == side3) {
-
+    if (side1 == side2 && side2 == side3) {
         cout << "The triangle is equilateral." << endl;
     }
-    
-    else if (side1 == side2 && side2 < side3) {
+    else if (side1 == side2 || side1 == side3 || side2 == side3) {
         cout << "The triangle is isosceles." << endl;
     }
-
-    else if (side1 < side2 && side2 < side3) {
+    else {
         cout << "The triangle is scalene." << endl;
     }
-   
-    return 0;
 
+    return 0;
 }
