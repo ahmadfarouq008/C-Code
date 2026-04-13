96a2f9557a632dee92c68e4f3b9cbd79ca31d7fc
ahmadfarouq008 <ahmadfarouq008@gmail.com>
2026-04-13 15:55:09 +0500

    int to char conversion

---
+++ int_to_char_conversion.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int num ;
+
+    cout << "enter number:";
+    cin >> num;
+
+    char ch = num;
+
+    cout << "ascii character =" << ch << endl;
+    cout << "back to integer=" << (int)ch << endl; 
+    
+
+
+}
+
+
+
