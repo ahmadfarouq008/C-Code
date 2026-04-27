f28a12f169678c25c36d716cd706773b199006f7
ahmadfarouq008 <ahmadfarouq008@gmail.com>
2026-04-28 01:13:16 +0500

    finding day of the week.

---
+++ 8.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int num=0;
+    cout << "Enter a number between 1 and 7:" ;
+    cin >> num;
+
+    if (num==1) {
+
+        cout << "The day is monday." << endl;
+    }
+    else if (num==2) {
+
+        cout << "The day is tuesday." << endl;
+    }
+    else if (num==3) {
+
+        cout << "The day is wednesday." << endl;
+    }
+    else if (num==4) {
+
+        cout << "The day is thursday." << endl;
+    }
+    else if (num==5) {
+
+        cout << "The day is friday." << endl;
+    }
+    else if (num==6) {
+
+        cout << "The day is saturday." << endl;
+    }
+    else if (num==7) {
+
+        cout << "The day is sunday." << endl;
+    }
+
+}
