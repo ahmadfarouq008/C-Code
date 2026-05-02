0409ff7f1657bcc1ff6918c5f2b1d564f971866a
ahmadfarouq008 <ahmadfarouq008@gmail.com>
2026-05-03 03:43:47 +0500

    .

---
+++ sum_of_odd_no.s_using_for_loop.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+
+    int n=0;
+    int sum=0;
+
+    cout << "Enter number:";
+    cin >>n;
+
+    for (int i=0;i<=n;i++) {
+
+        if (i % 2 != 0) {
+            sum+=i;
+        }
+    }
+    cout << "sum=" << sum << endl;
+    return 0;
+}
