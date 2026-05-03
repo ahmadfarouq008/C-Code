c66779ad078d501a88bec0b647e5ea78f2d90bff
ahmadfarouq008 <ahmadfarouq008@gmail.com>
2026-05-04 00:13:01 +0500

    .

---
+++ functions/sum_of_1_to_n_no.s.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int sumN(int n=0) {  //as the value of n is not fixed so we can take it as parameter.
+
+    int sum=0;
+
+    for (int i=0;i<=n;i++) { // here we wrote this code inside sumN function only one time but we made this for-loop code to run three times in below main function by just calling it three times. 
+        sum = sum + i;
+
+    }
+    return sum;
+
+}
+int main () {
+
+    cout << "sum of n numbers is: " << sumN(5) << endl;       
+    cout << "sum of n numbers is: " << sumN(10) << endl;
+    cout << "sum of n numbers is: " << sumN(57) << endl;
+
+    return 0; 
+
+}
