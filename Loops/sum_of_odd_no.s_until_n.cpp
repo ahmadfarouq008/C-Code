ec2beab1e408a61e9a3fecce7bcc0ac5f2e0036a
ahmadfarouq008 <ahmadfarouq008@gmail.com>
2026-05-10 10:38:42 +0500

    .

---
+++ Loops/sum_of_odd_no.s_until_n.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+ using namespace std;
+
+ int main () {
+    int n=0;
+    int sum=0;
+
+    cout << "ENter an integer:" ;
+    cin >> n;
+
+    for(int i=1;i<=n;i++) {
+
+        if (i%2!=0) {
+            sum+=i;
+        }
+
+    }
+    cout << "Sum of odd numbers is " << sum << endl;
+    return 0;
+
+ }
