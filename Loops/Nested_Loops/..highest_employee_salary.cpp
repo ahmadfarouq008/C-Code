b2809124e55b46a7108e10286249257d518c307d
ahmadfarouq008 <ahmadfarouq008@gmail.com>
2026-06-07 10:24:07 +0500

    .

---
+++ Loops/Nested_Loops/..highest_employee_salary.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std ;
+
+int main () {
+    int salary=0;
+    int total_sum=0;
+
+    int highest_salary=0;
+    int highest_employee=0;
+
+
+    for (int i=1; i<=3; i++) {
+
+        total_sum = 0 ;
+        cout << "Enter 6 months salary for employee " << i << ":" << endl;
+
+        for (int j=1; j<=6; j++) {
+            cout << "Month " << j << ":" << endl;
+            cin >> salary ;
+
+            total_sum += salary;
+        }
+        cout << "Employee " << i << " --> Total = " << total_sum << endl;
+
+        if (total_sum > highest_salary ) {
+            
+            highest_salary = total_sum;
+            highest_employee = i ; 
+        }   
+    }
+    cout << "Highest paid employee: Employee " << highest_employee << " with " << highest_salary << " yearly" << endl ; 
+    
+    return 0;    
+}
