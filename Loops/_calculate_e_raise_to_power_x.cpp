cc36ee7e5ab8efecf52dae73e1ac71fd229cc4c9
ahmadfarouq008 <ahmadfarouq008@gmail.com>
2026-06-08 20:10:06 +0500

    .

--- Loops/_calculate_e_raise_to_power_x.cpp
+++ Loops/_calculate_e_raise_to_power_x.cpp
@@ -16,12 +16,12 @@ int main () {
 
     for (int i=0; i<=10; i++) {
 
-        if (i>0) {  // we use this to print e^x = 1 in i=0.so it skips i=0 and make power=1 and factorial=1 as initiated. OR we can seprately cout e^0=1 and start with i=1. 
+        if (i>0) {                       // we use this to print e^x = 1 in i=0.so it skips i=0 and make power=1 and factorial=1 as initiated. OR we can seprately cout e^0=1 and start with i=1. 
             
-            power = power * x ; 
-            factorial = factorial * i ;
+            power = power * x ;          // Calculates x¹, x², x³, x⁴ ...
+            factorial = factorial * i ;  // Calculates 1!, 2!, 3!, 4! ...
         }
-        sum = sum + (power / factorial) ;
+        sum = sum + (power / factorial) ; // add above both terms.
     }
     cout << "e^" << x << " = " << sum <<  endl;  
     return 0;
