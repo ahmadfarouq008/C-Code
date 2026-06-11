65528dd4f2fc34b312c997a9deb38f9f5fd85454
ahmadfarouq008 <ahmadfarouq008@gmail.com>
2026-06-12 02:12:46 +0500

    .

---
+++ Loops/Nested_Loops/..right_triangle_no.s_form.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std ;
+
+int main () {
+    int n = 0;
+    int integer = 0;
+    int sum = 0;
+
+    cout << "Enter n : " ;
+    cin >> n; 
+
+    for (int i=1; i<=n; i++) {
+
+        for (int j=1; j<=i; j++) {
+            cout << j ;
+            
+        }
+        cout << endl;
+    }
+    return 0;
+}
